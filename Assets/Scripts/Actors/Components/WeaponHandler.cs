1afccec723b2d213f86b045af5d2ef7f2d993b6b
Mono2692 <nzubiria1@gmail.com>
2020-05-24 11:54:29 -0500

    Missing namespace.

--- Assets/Scripts/Actors/Components/WeaponHandler.cs
+++ Assets/Scripts/Actors/Components/WeaponHandler.cs
@@ -1,85 +1,89 @@
-﻿using EndGame.Test.Actors;
-using EndGame.Test.Events;
+﻿using EndGame.Test.Events;
+using EndGame.Test.Weapons;
 using UnityEngine;
 
-public class WeaponHandler : ActorComponent
+namespace EndGame.Test.Actors
 {
-    [SerializeField]
-    private WeaponFire weaponToShoot = null;
-    [SerializeField]
-    private Detector targeter = null;
-
-    public override void OnAwake(Actor _actor)
+    public class WeaponHandler : ActorComponent
     {
-        base.OnAwake(_actor);
+        [SerializeField]
+        private WeaponFire weaponToShoot = null;
+        [SerializeField]
+        private Detector targeter = null;
 
-        targeter = GetComponent<Detector>();
-    }
-
-    private void Start()
-    {
-        // TODO subscribe to on target in sight.
-        EventController.SubscribeToEvent(ActorEvents.ACTOR_COMMAND_RECEIVE, (args) => OnShootCommand((OnActorCommandReceiveEventArgs)args));
-    }
-
-    private void OnShootCommand(OnActorCommandReceiveEventArgs _args)
-    {
-        if (GetOwner == _args.actor)
+        public override void OnAwake(Actor _actor)
         {
-            if (_args.command.Equals(ActorCommands.Shoot))
+            base.OnAwake(_actor);
+
+            targeter = GetComponent<Detector>();
+        }
+
+        private void Start()
+        {
+            // TODO subscribe to on target in sight.
+            EventController.SubscribeToEvent(ActorEvents.ACTOR_COMMAND_RECEIVE, (args) => OnShootCommand((OnActorCommandReceiveEventArgs)args));
+        }
+
+        private void OnShootCommand(OnActorCommandReceiveEventArgs _args)
+        {
+            if (GetOwner == _args.actor)
             {
-                float inputValue = (float)_args.value;
-                if (inputValue > 0.0f)
+                if (_args.command.Equals(ActorCommands.Shoot))
                 {
-                    PullTrigger();
-                }
-                else
-                {
-                    ReleaseTrigger();
+                    float inputValue = (float)_args.value;
+                    if (inputValue > 0.0f)
+                    {
+                        PullTrigger();
+                    }
+                    else
+                    {
+                        ReleaseTrigger();
+                    }
                 }
             }
         }
-    }
 
-    private void PullTrigger()
-    {
-        OnActorPulledTrigger args = new OnActorPulledTrigger()
+        private void PullTrigger()
         {
-            actor = GetOwner
-        };
+            OnActorPulledTrigger args = new OnActorPulledTrigger()
+            {
+                actor = GetOwner
+            };
 
-        EventController.QueueEvent(ActorEvents.ACTOR_TRIGGER_PULLED, args);
-    }
+            EventController.QueueEvent(ActorEvents.ACTOR_TRIGGER_PULLED, args);
+        }
 
-    private void ReleaseTrigger()
-    {
-        OnActorReleasedTrigger args = new OnActorReleasedTrigger()
+        private void ReleaseTrigger()
         {
-            actor = GetOwner
-        };
+            OnActorReleasedTrigger args = new OnActorReleasedTrigger()
+            {
+                actor = GetOwner
+            };
 
-        EventController.QueueEvent(ActorEvents.ACTOR_TRIGGER_RELEASED, args);
-    }
+            EventController.QueueEvent(ActorEvents.ACTOR_TRIGGER_RELEASED, args);
+        }
 
-    /// <summary>
-    /// Called by an animation event at the start of the shooting animation.
-    /// </summary>
-    private void OnShootAnimationInit()
-    {
-        Vector3 targetDirection = targeter.GetTargetDirection;
-        weaponToShoot.FireWeapon(targetDirection);
-
-        OnActorFireWeapon args = new OnActorFireWeapon()
+        /// <summary>
+        /// Called by an animation event at the start of the shooting animation.
+        /// </summary>
+        private void OnShootAnimationInit()
         {
-            actor = GetOwner,
-            aimDirection = targetDirection
-        };
+            Vector3 targetDirection = targeter.GetTargetDirection;
+            weaponToShoot.FireWeapon(targetDirection);
 
-        EventController.QueueEvent(ActorEvents.ACTOR_FIRE_WEAPON, args);
+            OnActorFireWeapon args = new OnActorFireWeapon()
+            {
+                actor = GetOwner,
+                aimDirection = targetDirection
+            };
+
+            EventController.QueueEvent(ActorEvents.ACTOR_FIRE_WEAPON, args);
+        }
+
+        /// <summary>
+        /// Called by an animation event at the end of the shooting animation.
+        /// </summary>
+        private void OnFinishShootAnimtionEvent() => weaponToShoot.OnFinishShootAnimtionEvent();
     }
 
-    /// <summary>
-    /// Called by an animation event at the end of the shooting animation.
-    /// </summary>
-    private void OnFinishShootAnimtionEvent() => weaponToShoot.OnFinishShootAnimtionEvent();
 }
